cb7ac20f34ca33e1a4b1a9fbc19a99505170ff63
Wiktor Gut <94977771+Guccio163@users.noreply.github.com>
2023-08-03 22:52:47 +0200

    Fetching and showing songs repaired

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,24 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import GuessedButton from '../components/Buttons/GuessedButton.tsx';
 import SummaryPage from './SummaryPage.tsx';
 import 'bootstrap/dist/css/bootstrap.css';
 import { HomeButton } from '../components/Buttons/HomeButton.tsx';
 import { Song, getSongsShuffled, shuffleArray } from '../components/Lists/SongsList.tsx';
 
-export default function GamePanel() {
-  const [artist, setArtist] = useState(' ');
-  const [title, setTitle] = useState(' ');
-  // const [currentNum, setCurrentNum] = useState(0);
-  const [total, setTotal] = useState(0);
-  const [qCount, setQCount] = useState(-1);
-  const [questions, setQuestions] = useState<Song[]>([]);
-  const [recordArray, setRecordArray] = useState<string[][]>([]);
+type Records = {
+  tit: string;
+  art: string;
+  res: string;
+};
 
-  // function getRandomNumberInRange(min: number, max: number): number {
-  //   let ans = Math.floor(Math.random() * (max - min)) + min;
-  //   ans = ans === currentNum ? (ans + 1) % max : ans;
-  //   return ans;
-  // }
+export default function GamePanel() {
+  const [total, setTotal] = useState(0);
+  const [qCount, setQCount] = useState(0);
+  const [questions, setQuestions] = useState<Song[]>([]);
+  const [recordArray, setRecordArray] = useState<Records[]>([]);
 
   // function refreshData(myJson: string | any[]) {
   //   // ZMIEŃ DŁUGOŚĆ NA NP. 10
@@ -28,65 +25,32 @@ export default function GamePanel() {
   // }
 
   function addToList(tit: string, art: string, res: string) {
-    const rec: string[] = [tit, art, res];
+    const rec = { tit, art, res };
     setRecordArray([...recordArray, rec]);
-  }
-
-  function increaseTotal() {
-    setTotal(total + 1);
-  }
-
-  // function getData() {
-  //   fetch('songs.json', {
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       Accept: 'application/json',
-  //     },
-  //   })
-  //     .then((response) => {
-  //       console.log(response);
-  //       return response.json();
-  //     })
-  //     .then((myJson) => {
-  //       console.log(myJson);
-  //       refreshData(myJson);
-  //       setQCount(qCount + 1);
-  //     });
-  // }
-
-  function getNextQuestion() {
-    setQCount(qCount + 1);
-    setArtist(questions[qCount].artist);
-    setTitle(questions[qCount].title);
-    console.log(qCount, artist, title, questions[qCount]);
   }
 
   async function fetchSongs() {
     try {
-      setQuestions(await getSongsShuffled());
+      const qr = await getSongsShuffled();
+      setQuestions(qr);
+      console.log('qr', qr);
     } catch (error) {
       console.error('Błąd podczas pobierania danych:', error);
     }
-
-
   }
 
   useEffect(() => {
-    fetchSongs()
-      .then(() => {
-        getNextQuestion();
-      });
-    // getData();
+    fetchSongs();
   }, []);
 
   if (qCount < 4) {
     return (
       <div className="gamePanel">
-        {artist && artist.length > 0 && (
+        {questions[qCount]?.artist && questions[qCount].artist.length > 0 && (
           <div>
             <HomeButton />
-            <p className="title">{title}</p>
-            <p className="artist">{artist}</p>
+            <p className="title">{questions[qCount].title}</p>
+            <p className="artist">{questions[qCount].artist}</p>
           </div>
         )}
         <p className="total">
@@ -96,16 +60,18 @@ export default function GamePanel() {
           <GuessedButton
             option="wrong"
             onClick={() => {
-              getNextQuestion();
-              addToList(title, artist, 'f');
+              setQCount((prevIndex) => prevIndex + 1);
+
+              addToList(questions[qCount].title, questions[qCount].artist, 'f');
             }}
           />
           <GuessedButton
             option="right"
             onClick={() => {
-              getNextQuestion();
-              increaseTotal();
-              addToList(title, artist, 'r');
+              setQCount((prevIndex) => prevIndex + 1);
+
+              setTotal((total) => total + 1);
+              addToList(questions[qCount].title, questions[qCount].artist, 'r');
             }}
           />
         </div>{' '}
@@ -124,7 +90,7 @@ export default function GamePanel() {
           }}
           setTotal={setTotal}
           score={total}
-          questions={recordArray}
+          questions={[]}
         />
         <p className="signature"> Czółko v.0.1 </p>
       </>
